d80fba3b6782e0a9a2fc492fc6402d4b710d5444
datikken <tikken23@gmail.com>
2021-01-23 20:15:41 +0300

    earthquckes api

--- 11_json.py
+++ 11_json.py
@@ -9,11 +9,18 @@ def printResults(data):
     theJSON = json.loads(data)
     #print(json.dumps(theJSON, indent=4, sort_keys=True))
 
-    if 'associate' in theJSON['producttypes']:
-        pretty_print(theJSON['producttypes'])
+    if 'title' in theJSON['metadata']:
+        pretty_print(theJSON['metadata']['title'])
+    for i in theJSON['features']:
+        print(i['properties']['place'])
+    print('--------------------------')
+
+    for i in theJSON['features']:
+        if i['properties']['mag'] >= 6.0:
+            print("%2.1f" % i['properties']['mag'], i['properties']['place'], '* AHTUNG')
 
 def main():
-    urlData = 'https://earthquake.usgs.gov/fdsnws/event/1/application.json'
+    urlData = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02'
 
     webUrl = urllib.request.urlopen(urlData)
     print('http stat:', str(webUrl.getcode()))
